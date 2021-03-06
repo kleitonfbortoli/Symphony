8bad998a8129c128c1f4dfa88561ca15943c74da
Kleiton Franzon Bortoli <kleiton.bortoli@universo.univates.br>
2021-03-06 09:33:42 -0300

    adicionada novas tabelas novas no ORM

--- api/sql_app/recreate_database.py
+++ api/sql_app/recreate_database.py
@@ -1,7 +1,7 @@
 import database as database
 from database import *
 
-db.connect()
+Symphony_Db.connect()
 
-db.drop_tables([Pais,Estado,Cidade,Pessoa,Curso,Contrato])
-db.create_tables([Pais,Estado,Cidade,Pessoa,Curso,Contrato]) 
+Symphony_Db.drop_tables([Avaliacao_Matricula_Turma,Turma_Professor,Frequencia,Turma_Ocorencia,Periodo_Academico,Horario,Matricula_Turma,Avaliacao,Nota_Turma,Turma,Nota,Tipo_Nota,Matriz_Serie,Disciplina,Matricula_Serie,Serie,Pessoa,Cidade,Estado,Pais])
+Symphony_Db.create_tables([Pais,Estado,Cidade,Pessoa,Serie,Matricula_Serie,Disciplina,Matriz_Serie,Tipo_Nota,Nota,Turma,Nota_Turma,Avaliacao,Matricula_Turma,Horario,Periodo_Academico,Turma_Ocorencia,Frequencia,Turma_Professor,Avaliacao_Matricula_Turma]) 
